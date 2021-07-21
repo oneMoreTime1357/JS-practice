e91ba4add3f9690bb1ec901bc2558885351d57b5
dan yang <yangdan1357@gmail.com>
2021-07-21 19:36:31 +0800

    add deep copy file

---
+++ deepCopy.js
@@ -0,0 +1,29 @@
+/**
+ * 深拷贝
+ */
+
+function deepCopy (obj) {
+  const res = Array.isArray(obj) ? [] : {}
+
+  for (const key in obj) {
+    if (typeof res === 'object') {
+      deepCopy(obj[key])
+    } else {
+      res[key] = obj[key]
+    }
+  }
+  return res
+}
+
+// 解决循环引用的问题
+
+var obj = {
+	a: 1,
+  b: 2
+}
+obj.e = obj
+console.log(obj) // 不会报错
+
+const objCopy = deepCopy(obj)
+console.log(objCopy) // 报错栈溢出
+
